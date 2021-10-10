954dd515217371315a72839fd7d54c2e3f6fb142
MiloPeregrin <milanperegrin@gmail.com>
2021-10-10 08:50:39 +0200

    1.49 improved profile page

--- src/components/reusable/UserProfile.js
+++ src/components/reusable/UserProfile.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import classes from "./UserProfile.module.css";
 import StateContext from "../../store/state-context";
-import { Typography, Image } from "antd";
+import { Typography, Image, Card } from "antd";
 import {
   GithubOutlined,
   MailOutlined,
@@ -12,44 +12,47 @@ import {
 const UserProfile = () => {
   const context = useContext(StateContext);
   return (
-    <div className={classes.container}>
-      <Image
-        className={classes.profilePicture}
-        preview={false}
-        src={context.user.avatar_url}
-      ></Image>
-      <span className={"userName"}>
-        <Typography.Title level={3}>
-          {context.user.name || context.user.login}
-        </Typography.Title>
-      </span>
-      <div className={classes.userInfo}>
-        <span>
-          <GithubOutlined />
-          <strong> Github : </strong>{" "}
-          <a href={context.user.html_url} target="_blank" rel="noreferrer">
-            {context.user.html_url}
-          </a>
+    <div className={classes.cardContainer}>
+      <Card
+        bodyStyle={{ padding: 15 }}
+        hoverable
+        style={{ width: 300 }}
+        cover={<img src={context.user.avatar_url} />}
+      >
+        <span className={classes.cardTitle}>
+          <Typography.Title level={4}>
+            {context.user.name || context.user.login}
+          </Typography.Title>
         </span>
-        <span>
-          <MailOutlined />
-          <strong> Email : </strong>{" "}
-          <a href={context.user.email} target="_blank" rel="noreferrer">
-            {context.user.email}
-          </a>
-        </span>
-        <span>
-          <FormOutlined />
-          <strong> Blog : </strong>{" "}
-          <a href={context.user.blog} target="_blank" rel="noreferrer">
-            {context.user.blog}
-          </a>
-        </span>
-        <span>
-          <EnvironmentOutlined />
-          <strong> Location : </strong> {context.user.location}
-        </span>
-      </div>
+
+        <div className={classes.userInfo}>
+          <span>
+            <GithubOutlined />
+            <strong> Git: </strong>
+            <a href={context.user.html_url} target="_blank" rel="noreferrer">
+              {context.user.html_url}
+            </a>
+          </span>
+          <span>
+            <MailOutlined />
+            <strong> Email: </strong>
+            <a href={context.user.email} target="_blank" rel="noreferrer">
+              {context.user.email}
+            </a>
+          </span>
+          <span>
+            <FormOutlined />
+            <strong> Blog: </strong>
+            <a href={context.user.blog} target="_blank" rel="noreferrer">
+              {context.user.blog}
+            </a>
+          </span>
+          <span>
+            <EnvironmentOutlined />
+            <strong> Location: </strong> {context.user.location}
+          </span>
+        </div>
+      </Card>
     </div>
   );
 };
